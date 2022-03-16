d267353e01391afce62bd573c547a6b25608605e
Mahmoud Metwally <mmustafametwally@gmail.com>
2022-03-16 21:22:13 +0200

    upload app

---
+++ task/task/doctype/name_concatenation/name_concatenation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Mahmoud Metwally and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Name Concatenation', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ task/task/doctype/name_concatenation/test_name_concatenation.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Mahmoud Metwally and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestNameConcatenation(FrappeTestCase):
+	pass
